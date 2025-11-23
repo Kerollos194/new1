16023596e4a5a97526f3131f4284b66214141862
Kerollos194 <kerosamer400@gmail.com>
2025-11-23 15:48:24 +0200

    initial setup
    -

---
+++ app-cal.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file app-cal.cpp
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-11-23
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#include <iostream>
+using namespace std;
+
+int add(int a,int b);
+int substract(int a,int b);
+
+int main(){
+
+     return 0;
+
+}
